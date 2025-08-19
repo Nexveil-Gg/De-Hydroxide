2922cd10c69c4af6b54dd386f33a474a235fa745
ryte-e <turrribababa@gmail.com>
2025-08-19 11:00:25 +0300

    Update Prompt.lua

--- ui/controls/Prompt.lua
+++ ui/controls/Prompt.lua
@@ -27,4 +27,16 @@ function Prompt.hide(prompt)
     currentPrompt = nil
 end
 
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Assets = import("rbxassetid://5042114982") 
+local ModifyConstant = Assets.ModifyUpvalue:Clone()
+
+ModifyConstant.Name = "ModifyConstant"
+
+ModifyConstant.Inner.Title.Text = "Modify Constant"
+
+return ModifyConstant
+
+
 return Prompt
