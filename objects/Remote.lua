945812fd8883489c474a951260bffed151ec8e27
Nexveil-Gg <aloabifisabi@gmail.com>
2025-11-26 20:32:13 +0300

    Update Remote.lua

--- objects/Remote.lua
+++ objects/Remote.lua
@@ -1,24 +1,56 @@
 local Remote = {}
 
+local function hasWeirdCharacters(name)
+    if not name or name == "" then
+        return true
+    end
+    
+    for i = 1, #name do
+        local byte = string.byte(name, i)
+        if byte > 127 or byte < 32 then
+            return true
+        end
+    end
+    
+    return false
+end
+
 function Remote.new(instance)
     local remote = {}
 
     remote.Instance = instance
+    remote.Name = instance.Name
+    remote.ClassName = instance.ClassName
+    remote.Parent = instance.Parent
+    remote.FullName = instance.Parent and instance:GetFullName() or "nil." .. instance.Name
+    
+    remote.IsBindable = instance.ClassName == "BindableEvent" or instance.ClassName == "BindableFunction"
+    remote.IsRemote = instance.ClassName == "RemoteEvent" or instance.ClassName == "RemoteFunction"
+    remote.IsFunction = instance.ClassName == "RemoteFunction" or instance.ClassName == "BindableFunction"
+    remote.IsEvent = instance.ClassName == "RemoteEvent" or instance.ClassName == "BindableEvent"
+    
+    remote.HasWeirdName = hasWeirdCharacters(instance.Name)
+    remote.IsNilParent = instance.Parent == nil
+    
     remote.Logs = {}
     remote.Calls = 0
     remote.Blocked = false
     remote.Ignored = false
+    remote.BlockedArgs = {}
+    remote.IgnoredArgs = {}
+    
     remote.Clear = Remote.clear
     remote.Block = Remote.block
     remote.Ignore = Remote.ignore
-    remote.BlockedArgs = {}
-    remote.IgnoredArgs = {}
     remote.BlockArg = Remote.blockArg
     remote.IgnoreArg = Remote.ignoreArg
+    remote.UnblockArg = Remote.unblockArg
+    remote.UnignoreArg = Remote.unignoreArg
     remote.AreArgsBlocked = Remote.areArgsBlocked
     remote.AreArgsIgnored = Remote.areArgsIgnored
     remote.IncrementCalls = Remote.incrementCalls
     remote.DecrementCalls = Remote.decrementCalls
+    remote.GetInfo = Remote.getInfo
 
     return remote
 end
@@ -64,11 +96,115 @@ function Remote.ignoreArg(remote, index, value, byType)
             types = {},
             values = {}
         }
-
         ignoredArgs[index] = indexIgnore
     end
 
     if byType then
+        indexIgnore.types[value] = true
+    else
+        indexIgnore.values[value] = true
+    end
+end
+
+function Remote.unblockArg(remote, index, value, byType)
+    local blockedArgs = remote.BlockedArgs
+    local blockedIndex = blockedArgs[index]
+
+    if blockedIndex then
+        if byType then
+            blockedIndex.types[value] = nil
+        else
+            blockedIndex.values[value] = nil
+        end
+    end
+end
+
+function Remote.unignoreArg(remote, index, value, byType)
+    local ignoredArgs = remote.IgnoredArgs
+    local indexIgnore = ignoredArgs[index]
+
+    if indexIgnore then
+        if byType then
+            indexIgnore.types[value] = nil
+        else
+            indexIgnore.values[value] = nil
+        end
+    end
+end
+
+function Remote.areArgsBlocked(remote, args)
+    local blockedArgs = remote.BlockedArgs
+
+    for index, value in pairs(args) do
+        local indexBlock = blockedArgs[index]
+        
+        if indexBlock then
+            if indexBlock.types[typeof(value)] then
+                return true
+            end
+            if indexBlock.values[value] ~= nil then
+                return true
+            end
+        end
+    end
+    
+    return false
+end
+
+function Remote.areArgsIgnored(remote, args)
+    local ignoredArgs = remote.IgnoredArgs
+
+    for index, value in pairs(args) do
+        local indexIgnore = ignoredArgs[index]
+
+        if indexIgnore then
+            if indexIgnore.types[typeof(value)] then
+                return true
+            end
+            if indexIgnore.values[value] ~= nil then
+                return true
+            end
+        end
+    end
+    
+    return false
+end
+
+function Remote.incrementCalls(remote, callData)
+    remote.Calls = remote.Calls + 1
+    table.insert(remote.Logs, callData)
+end
+
+function Remote.decrementCalls(remote, callData)
+    local logs = remote.Logs
+
+    remote.Calls = remote.Calls - 1
+    local index = table.find(logs, callData)
+    if index then
+        table.remove(logs, index)
+    end
+end
+
+function Remote.getInfo(remote)
+    return {
+        Name = remote.Name,
+        ClassName = remote.ClassName,
+        FullName = remote.FullName,
+        IsBindable = remote.IsBindable,
+        IsRemote = remote.IsRemote,
+        IsFunction = remote.IsFunction,
+        IsEvent = remote.IsEvent,
+        HasWeirdName = remote.HasWeirdName,
+        IsNilParent = remote.IsNilParent,
+        Calls = remote.Calls,
+        Blocked = remote.Blocked,
+        Ignored = remote.Ignored
+    }
+end
+
+Remote.HasWeirdCharacters = hasWeirdCharacters
+
+return Remote    if byType then
         indexIgnore.types[value] = true
     else
         indexIgnore.values[value] = true
